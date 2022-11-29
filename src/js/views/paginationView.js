11f4822b5d4bedae0efa7d59afd03a893d8aaea3
suhuq007@gmail.com <suhuq007@gmail.com>
2022-11-29 22:31:19 +0530

    added pagination markup

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -1,4 +1,16 @@
-importb
+import View from './view.js';
+import { data } from '../model.js'
+
 class Pagination extends View {
+  _parentElem = document.querySelector('[data-pagination-btns-container]')
   
+  generateNextBtnMarkup() {
+    const html = `
+    <button data-goto="${data.page + 1}"> Next </button>`
+  }
+
+  generatePrevBtnMarkup() {
+    const html = `
+    <button data-goto="${data.page - 1}"> Next </button>`
+  }
 }
