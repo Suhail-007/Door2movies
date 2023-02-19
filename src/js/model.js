207f3e3fe820957dafe0b3bc2e22ead2757a6430
suhuq007@gmail.com <suhuq007@gmail.com>
2023-02-19 21:00:54 +0530

    not appending year on footer anymore

--- src/js/model.js
+++ src/js/model.js
@@ -136,5 +136,5 @@ export const HistoryBackForward = function(handler) {
 export const copyRightYear = function() {
   const yearElem = document.querySelector('[data-copyright-year]');
   const year = new Date().getFullYear();
-  yearElem.textContent += year;
+  yearElem.textContent = year;
 }
